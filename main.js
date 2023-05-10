61dd8f1848e6d6fa83a50e8271ba2c3f8f7fab9d
jaxongir001 <jaxongir4990@gmail.com>
2023-05-10 17:11:41 +0500

    java

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let yourMoney = +prompt(" Xisobingizda bor pulni kiriting ...!"),
+  expensive = 9732480;
+
+
+if (yourMoney >= expensive) {
+  console.log("oq yo'l Alisher ");
+
+} else {
+  console.log("Alisher,ozgina sabr qilishiz kerak ekan");
+}
